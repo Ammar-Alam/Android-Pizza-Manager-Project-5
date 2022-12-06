4027f5dc59e7cb8fe0aa3db710972d52cc4cda9e
Ammar-Alam <ammaralam37@gmail.com>
2022-12-06 15:59:18 -0500

    Added images and created activity views

---
+++ app/src/main/java/com/application/androidpizzamanagerproject5/NYOrderActivity.java
@@ -0,0 +1,14 @@
+package com.application.androidpizzamanagerproject5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NYOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nyorder);
+    }
+}
