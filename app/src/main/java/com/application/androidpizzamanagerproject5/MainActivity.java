633503b7ce069a40c0c08b9fc329eea71de26f03
Ammar-Alam <ammaralam37@gmail.com>
2022-12-06 16:02:16 -0500

    Implemented button functionality

--- app/src/main/java/com/application/androidpizzamanagerproject5/MainActivity.java
+++ app/src/main/java/com/application/androidpizzamanagerproject5/MainActivity.java
@@ -2,13 +2,60 @@ package com.application.androidpizzamanagerproject5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import pizzaclasses.ChicagoPizza;
+import pizzaclasses.Order;
+import pizzaclasses.Pizza;
+import pizzaclasses.Size;
+import pizzaclasses.StoreOrders;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static StoreOrders storeOrders = new StoreOrders();
+    public static Order currentOrder = new Order(storeOrders.getNextOrderNumber());
+    /**
+     * Initialize MainActivity
+     * @param savedInstanceState Bundle object
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Launches NY Order Activity
+     * @param view View
+     */
+    public void launchNYOrderActivity(View view){
+        Intent intent = new Intent(this, NYOrderActivity.class);
+        startActivity(intent);
+    }
+    /**
+     * Launches Chicago Order Activity
+     * @param view View
+     */
+    public void launchChicagoOrderActivity(View view){
+        Intent intent = new Intent(this, ChicagoOrderActivity.class);
+        startActivity(intent);
+    }
+    /**
+     * Launches Current Order Activity
+     * @param view View
+     */
+    public void launchCurrentOrderActivity(View view){
+        Intent intent = new Intent(this, CurrentOrderActivity.class);
+        startActivity(intent);
+    }
+    /**
+     * Launches Store Orders Activity
+     * @param view View
+     */
+    public void launchStoreOrdersActivity(View view){
+        Intent intent = new Intent(this, StoreOrdersActivity.class);
+        startActivity(intent);
     }
 }
