394d8e05b202eb17068cd3b7bfd7d5a8b78a5013
Nikhil <nikhilgolla101@Gmail.com>
2022-12-06 20:18:48 -0500

    "Made changes to New York Style Pizza activity and xml files"

--- app/src/main/java/com/application/androidpizzamanagerproject5/ChicagoOrderActivity.java
+++ app/src/main/java/com/application/androidpizzamanagerproject5/ChicagoOrderActivity.java
@@ -28,7 +28,6 @@ import pizzaclasses.Pizza;
 import pizzaclasses.PizzaFactory;
 import pizzaclasses.Size;
 import pizzaclasses.Topping;
-//this is a comment
 
 public class ChicagoOrderActivity extends AppCompatActivity {
     /**
